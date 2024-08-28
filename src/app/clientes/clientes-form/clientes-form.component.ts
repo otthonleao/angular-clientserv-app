d764ec08c862aa988a176b936e5b9c3026aabadb
otthonleao <otthonleao@hotmail.com>
2024-08-28 02:02:38 -0400

    feat(clientes): add alert for sucess and error message with error handling

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -10,6 +10,8 @@ import { ClientesService } from '../../clientes.service';
 export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente;
+  success: boolean = false;
+  errors: String[];
 
   constructor( private service: ClientesService ) {
     this.cliente = new Cliente();
@@ -20,7 +22,14 @@ export class ClientesFormComponent implements OnInit {
   onSubmit() {
     this.service.salvar(this.cliente)
       .subscribe(response => {
+        this.success = true;
+        this.errors = null;
+        this.cliente = response; // Preenche os campos do form com o cliente salvo
         console.log(response);
+    }, errorResponse => {
+      this.errors = errorResponse.error.errors;
+      this.success = false;
+      console.log(errorResponse.error.errors);
     });
   }
 }
