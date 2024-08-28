4904a73c6fd726df2a3a94da957d08086cc236b8
otthonleao <otthonleao@hotmail.com>
2024-08-28 00:24:27 -0400

    chore(clientes): add data-binding in the form

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../cliente';
 
 @Component({
   selector: 'app-clientes-form',
   templateUrl: './clientes-form.component.html',
-  styleUrls: ['./clientes-form.component.css']
+  styleUrls: ['./clientes-form.component.css'],
 })
 export class ClientesFormComponent implements OnInit {
 
-  constructor() { }
+  cliente: Cliente;
 
-  ngOnInit(): void {
+  constructor() {
+    this.cliente = new Cliente();
   }
 
+  ngOnInit(): void {}
+
+  clicar() {
+    console.log('Clicou no botão');
+    console.log(this.cliente);
+  }
 }
