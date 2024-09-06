d06c792de83be4d725f90b921ed42cdb47e669dd
otthonleao <otthonleao@hotmail.com>
2024-09-06 02:05:40 -0400

    chore(clientes): add 'Atualizar' && 'Editar' com catch client.id and routes in the buttons

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -13,7 +13,10 @@ export class ClientesService {
 
   salvar(cliente: Cliente) : Observable<Cliente> {
     return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
+  }
 
+  atualizar(cliente: Cliente) : Observable<Cliente> {
+    return this.http.put<Cliente>(`http://localhost:8080/api/clientes/${cliente.id}`, cliente);
   }
 
   // getClientes() : Cliente[] {
@@ -27,5 +30,9 @@ export class ClientesService {
 
   getClientes() : Observable<Cliente[]> {
     return this.http.get<Cliente[]>('http://localhost:8080/api/clientes');
+  }
+
+  getClienteById(id:number) : Observable<Cliente> {
+    return this.http.get<any>(`http://localhost:8080/api/clientes/${id}`);
   }
 }
--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Cliente } from '../cliente';
 import { ClientesService } from '../../clientes.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-clientes-form',
@@ -10,32 +11,63 @@ import { ClientesService } from '../../clientes.service';
   styleUrls: ['./clientes-form.component.css'],
 })
 export class ClientesFormComponent implements OnInit {
-
   cliente: Cliente;
   success: boolean = false;
   errors: String[];
+  id: number;
 
-  constructor( private service: ClientesService, private router: Router ) {
+  constructor(
+    private service: ClientesService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
     this.cliente = new Cliente();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    let params : Observable<Params>= this.activatedRoute.params;
+    params.subscribe(urlParams => {
+
+      this.id = urlParams['id'];
+      if(this.id){
+        this.service
+          .getClienteById(this.id)
+          .subscribe(
+            response => this.cliente = response,
+            errorResponse => this.cliente = new Cliente()
+        )
+      }
+    });
+  }
 
   voltarParaListagem() {
     this.router.navigate(['/clientes-lista']);
   }
 
   onSubmit() {
-    this.service.salvar(this.cliente)
-      .subscribe(response => {
-        this.success = true;
-        this.errors = null;
-        this.cliente = response; // Preenche os campos do form com o cliente salvo
-        console.log(response);
-    }, errorResponse => {
-      this.errors = errorResponse.error.errors;
-      this.success = false;
-      console.log(errorResponse.error.errors);
-    });
+    if (this.id) {
+      this.service
+        .atualizar(this.cliente)
+        .subscribe(Response => {
+          this.success = true;
+          this.errors = null;
+        }, errorResponse => {
+          this.errors = ['Erro ao atualizar o cliente'];
+        });
+    } else {
+      this.service.salvar(this.cliente).subscribe(
+        (response) => {
+          this.success = true;
+          this.errors = null;
+          this.cliente = response; // Preenche os campos do form com o cliente salvo
+          console.log(response);
+        },
+        (errorResponse) => {
+          this.errors = errorResponse.error.errors;
+          this.success = false;
+          console.log(errorResponse.error.errors);
+        }
+      );
+    }
   }
 }
