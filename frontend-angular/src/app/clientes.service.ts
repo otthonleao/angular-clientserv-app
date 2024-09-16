4be085efa692e94eff7599738d3bc27009434484
Otthon Leão <otthonleao@hotmail.com>
2024-09-16 18:57:40 -0400

    feat(env): add apiBaseURL and change services

--- frontend-angular/src/app/clientes.service.ts
+++ frontend-angular/src/app/clientes.service.ts
@@ -2,21 +2,27 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './clientes/cliente';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
+  apiURL: string = environment.apiBaseURL + '/api/clientes';
+
   constructor( private http: HttpClient ) {
   }
 
   salvar(cliente: Cliente) : Observable<Cliente> {
-    return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
+    // return this.http.post<Cliente>(`http://localhost:8080/api/clientes`, cliente);
+    return this.http.post<Cliente>(`${this.apiURL}`, cliente);
+
   }
 
   atualizar(cliente: Cliente) : Observable<Cliente> {
-    return this.http.put<Cliente>(`http://localhost:8080/api/clientes/${cliente.id}`, cliente);
+    // return this.http.put<Cliente>(`http://localhost:8080/api/clientes/${cliente.id}`, cliente);
+    return this.http.put<Cliente>(`${this.apiURL}/${cliente.id}`, cliente);
   }
 
   // getClientes() : Cliente[] {
@@ -29,14 +35,14 @@ export class ClientesService {
   // }
 
   getClientes() : Observable<Cliente[]> {
-    return this.http.get<Cliente[]>('http://localhost:8080/api/clientes');
+    return this.http.get<Cliente[]>(`${this.apiURL}`);
   }
 
   getClienteById(id:number) : Observable<Cliente> {
-    return this.http.get<any>(`http://localhost:8080/api/clientes/${id}`);
+    return this.http.get<any>(`${this.apiURL}/${id}`);
   }
 
   deletar(cliente: Cliente) : Observable<any> {
-    return this.http.delete<any>(`http://localhost:8080/api/clientes/${cliente.id}`);
+    return this.http.delete<any>(`${this.apiURL}/${cliente.id}`);
   }
 }
--- frontend-angular/src/app/servico-prestado.service.ts
+++ frontend-angular/src/app/servico-prestado.service.ts
@@ -2,16 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ServicoPrestado } from './servico-prestado/servicoPrestado';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicoPrestadoService {
 
+  apiURL: string = environment.apiBaseURL;
+
   constructor(private http: HttpClient) {}
 
   salvar(servicoPrestado: ServicoPrestado) : Observable<ServicoPrestado> {
-    return this.http.post<ServicoPrestado>('http://localhost:8080/api/servicos-prestados', servicoPrestado);
+    // return this.http.post<ServicoPrestado>(`${this.apiURL}/api/servicos-prestados`, servicoPrestado);
+    return this.http.post<ServicoPrestado>(`http://localhost:8080/api/servicos-prestados`, servicoPrestado);
   }
 
 }
