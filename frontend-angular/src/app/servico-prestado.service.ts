d31015a75693d933cfbf88fe970f6dab6accb5aa
Otthon Leão <otthonleao@hotmail.com>
2024-09-16 23:06:57 -0400

    feat(front): add Consultar servicos prestados

--- frontend-angular/src/app/servico-prestado.service.ts
+++ frontend-angular/src/app/servico-prestado.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { ServicoPrestado } from './servico-prestado/servicoPrestado';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { ServicoPrestadoBusca } from './servico-prestado/servico-prestado-lista/servicoPrestadoBusca';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +15,16 @@ export class ServicoPrestadoService {
   constructor(private http: HttpClient) {}
 
   salvar(servicoPrestado: ServicoPrestado) : Observable<ServicoPrestado> {
-    // return this.http.post<ServicoPrestado>(`${this.apiURL}/api/servicos-prestados`, servicoPrestado);
-    return this.http.post<ServicoPrestado>(`http://localhost:8080/api/servicos-prestados`, servicoPrestado);
+    return this.http.post<ServicoPrestado>(`${this.apiURL}/api/servicos-prestados`, servicoPrestado);
+  }
+
+  buscar(nome: string, mes: number) : Observable<ServicoPrestadoBusca[]> {
+    const httpParams = new HttpParams()
+      .set("nome", nome)
+      .set("mes", mes ?  mes.toString() : '');
+
+    const url = this.apiURL + "?" + httpParams.toString();
+    return this.http.get<any>(url);
   }
 
 }
