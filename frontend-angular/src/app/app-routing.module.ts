d46047a03e37f4d442611dc44ff4583d0d4db47a
Otthon Leão <otthonleao@hotmail.com>
2024-09-17 21:17:43 -0400

    refactor(front): separando a página de login do resto do sistema

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { ClientesFormComponent } from './clientes/clientes-form/clientes-form.component';
 import { ClientesListaComponent } from './clientes/clientes-lista/clientes-lista.component';
 import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'clientes-form', component: ClientesFormComponent },
-  { path: 'clientes-lista', component: ClientesListaComponent }
+  { path: 'home', component: HomeComponent },
+  { path: '', component: LayoutComponent }
+  // { path: 'clientes-form', component: ClientesFormComponent },
+  // { path: 'clientes-lista', component: ClientesListaComponent }
 ];
 
 @NgModule({
