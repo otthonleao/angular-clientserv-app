688315fda7ee97ec44cd9d6ea74bfaf6f82ad18b
Otthon Leão <otthonleao@hotmail.com>
2024-09-18 03:23:08 -0400

    feat(back/login): add OAuth2 initial configuration

---
+++ backend-spring/src/main/java/dev/otthon/clientserv/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package dev.otthon.clientserv.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableResourceServer
+@EnableAuthorizationServer
+public class SecurityConfig extends WebSecurityConfiguration {
+}
